8b1aee6de06a984b757264992359b8882220ea55
V.H. Belvadi <hello@vhbelvadi.com>
2016-03-25 22:13:45 +0530

    Fixed wrong menu call

--- page.php
+++ page.php
@@ -18,8 +18,8 @@
 		<div class="all_posts"><a href="<?php bloginfo('url'); ?>/archive/">Read something</a>
 		</div> <!-- /end .all_posts -->
 		
-		<div class="subscribe"><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></div>
-		<div class="footer"><?php wp_nav_menu( array( 'theme_location' => 'social-menu' ) ); ?></div>
+		<div class="subscribe"><?php wp_nav_menu( array( 'theme_location' => 'social-menu' ) ); ?></div>
+		<div class="footer"><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></div>
 
 	</div>  <!-- /end .container -->
 
