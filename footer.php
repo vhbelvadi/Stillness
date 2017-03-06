bdfbadaeea3a1ded71bdf9e30bb4d15598253c53
V.H. Belvadi <hello@vhbelvadi.com>
2017-03-06 22:06:32 +0530

    Responsiveness support
    Added a label class for future development and defined variables for the responsive navigation script.

--- footer.php
+++ footer.php
@@ -2,11 +2,19 @@
 		</div> <!-- /end .all_posts -->
 		
 		<div class="subscribe"><?php wp_nav_menu( array( 'theme_location' => 'social-menu' ) ); ?></div>
+<label for="nav"></label>
 		<div class="footer"><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></div>
 
 	</div>  <!-- /end .container -->
 
 	<?php wp_footer(); ?>
+	
+<script>
+  var nav = responsiveNav(".footer");
+  var nav = responsiveNav(".subscribe", { 
+  label: "More", // String: Label for the navigation toggle
+});
+</script>
 
 </body>
 </html>
